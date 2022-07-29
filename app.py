6b4037a34390ac4c95739ff1063b6c3ad375d409
jblanquicett92 <jorgeabm1992@gmail.com>
2022-07-29 09:51:00 -0500

    feat(app): generate a flask app to read a text and find repeated words

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from flask import Flask
+from flask import Flask, request
+from flask_api import status
+
 app = Flask(__name__)
 
-@app.route('/status')
-def status():
-    return {'msg': 'OK'}, 200
+@app.route('/counter_common_word')
+def counter_common_word():
+
+    word = request.args.get('word')
+
+    custom_file = open("msg.txt", "r")
+    readable_file = custom_file.read()
+    readable_file_lower = readable_file.lower()
+    counter = readable_file_lower.count(word)
+
+    return {'counter': counter}, status.HTTP_200_OK
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=9000, debug=True)
