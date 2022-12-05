9538c83305b72a196f57af1eebd4d53279319a61
jblanquicett92 <jorgeabm1992@gmail.com>
2022-12-04 22:40:18 -0600

    feat(s3): s3

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c$xrl^&2qg^9g_i98nliibt2_00%u*7blqe2gly(kqn!y9y332'
+SECRET_KEY = 'django-insecure-*2(v6=$ro@*jw1*8_6g@1nh3eiyzcwheuu##5b1*-w)vr+0m%y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
